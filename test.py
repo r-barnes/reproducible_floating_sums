6c1ef3aa8da509d66d9424c4327cf2aada231485
Richard Barnes <rbarnes@umn.edu>
2021-05-26 00:14:16 -0600

    Make PyTorch lib

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import torch
+import torch.cuda.profiler as profiler
 torch.ops.load_library("build/libdeterministic.so")
 
 # a = torch.zeros((4,5), dtype=torch.float).cuda()
@@ -12,17 +13,25 @@ torch.ops.load_library("build/libdeterministic.so")
 
 #a = torch.randint(0, 10, (3,3,3), dtype=torch.float).cuda()
 
-# a = torch.zeros((3,3,3), dtype=torch.float).cuda()
+# a = torch.ones((3,3,3), dtype=torch.float).cuda()
 # b = torch.ones((3,3,3), dtype=torch.float).cuda()
 # index = torch.tensor([2,0,2]).cuda()
 
-D0 = 20
-a = torch.randint(0, 10, (D0,30,400,500), dtype=torch.float).cuda()
-b = torch.randint(0, 10, (D0,30,400,500), dtype=torch.float).cuda()
-index = torch.randint(0, D0, (D0, ), dtype=torch.int).cuda()
+dim = 1
+shape = (20,30,400,500)
+a = torch.randint(0, 10, shape, dtype=torch.float).cuda()
+b = torch.randint(0, 10, shape, dtype=torch.float).cuda()
+index = torch.randint(0, shape[dim], (shape[dim], ), dtype=torch.long).cuda()
 
-current = a.clone().index_add_(0, index, b)
-new = torch.ops.my_ops.index_add_deterministic(a, index, b)
-print("Correct\n", current)
-print("Mine\n", new)
-print("Equal?", torch.all(torch.eq(current, new)).cpu())
+current = a.clone().index_add_(dim, index, b)
+
+# new = torch.ops.my_ops.index_add_deterministic(a, index, b)
+
+profiler.start()
+new = torch.ops.my_ops.index_add_deterministic_yuguo(a, dim, index, b)
+profiler.stop()
+
+# print("Correct\n", current)
+# print("Mine\n", new)
+print("Exact Equal?", torch.all(torch.eq(current, new)).cpu())
+print("Close?", torch.allclose(current, new))
